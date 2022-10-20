70e22f7a18a0f2c44613c244af6df974867e79d4
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-10-20 18:25:14 -0500

    Load first project as test

--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,11 @@
 const projectDetails = [
   {
     id: 1,
-    title: "Project 1",
+    title: "Bombay Bicicle Club - Challenge Page",
     description:
-      "This is my portfolio website. It is built with React and uses React Hooks. It is hosted on Netlify.",
-    image: "https://i.imgur.com/0Z0Z7Zu.png",
-    link: "XXX",
+      "The very first website i made, its a fanpage for the band Bombay Bicycle Club. It is made only using HTML and CSS, and its quite simple. It was created as a challenge, and the time to complete the project was limited to 2 hours or less",
+    image: "http://live.staticflickr.com/65535/52442831290_73fd0f471f_c.jpg",
+    link: "http://github.com/alexpuente0/challenges",
   },
   {
     id: 2,
