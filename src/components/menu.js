96cae64db07f52246c841b8155455433a3b4b9b9
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-11-05 19:45:58 -0600

    additional changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import '../App.css';
+import "../App.css";
 import { BubblyLink } from "react-bubbly-transitions";
 
 const Menu = () => {
-
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -12,107 +11,89 @@ const Menu = () => {
 
   return (
     <>
-      {/* <div
-        className="hamburger"
-        id="hamburger"
-        role="presentation"
-        onClick={handleHamburgerClick}
-      >
-        <div className="slice" />
-      </div> */}
       <div className={menuOpen ? "open" : null}>
-        <nav className="sidebar">
-          <div className="sidebar-inner">
-            <header className="sidebar-header">
-              <button
-                type="button"
-                className="sidebar-burger"
-                onClick={toggleSidebar}
-              ></button>
-              <img
-                src="../assets/blocklord-logo.png"
-                className="sidebar-logo"
-                alt="logo"
-              />
-            </header>
-            <nav className="sidebar-menu">
-              <BubblyLink
-                colorStart="#162030"
-                colorEnd="#0d1117"
-                onClick={toggleSidebar}
-                to="/"
-              >
-                <img src={require("../assets/home.png")} alt="home" />
-                Home
-              </BubblyLink>
-              <BubblyLink
-                colorStart="#162030"
-                colorEnd="#0d1117"
-                onClick={toggleSidebar}
-                to="/about"
-              >
-                <img src={require("../assets/user.png")} alt="home" />
-                About
-              </BubblyLink>
-              <BubblyLink
-                colorStart="#162030"
-                colorEnd="#0d1117"
-                onClick={toggleSidebar}
-                to="/stack"
-              >
-                <img src={require("../assets/icon-levels.png")} alt="home" />
-                Toolbox
-              </BubblyLink>
-              <BubblyLink
-                colorStart="#162030"
-                colorEnd="#0d1117"
-                onClick={toggleSidebar}
-                to="/projects"
-              >
-                <img src={require("../assets/icon-home.png")} alt="home" />
-                Projects
-              </BubblyLink>
-              <></>
-            </nav>
-          </div>
-        </nav>
+        <button type="button" className="burger" onClick={toggleSidebar} />
+        <img className="logo" src={require("../assets/image.png")} alt="logo" />
+        <div className="sidebar">
+          <header className="sidebar-header" />
+
+          <nav className="sidebar-menu">
+            <BubblyLink
+              colorStart="#162030"
+              colorEnd="#0d1117"
+              onClick={toggleSidebar}
+              to="/"
+            >
+              <img src={require("../assets/image-home.png")} alt="home" />
+              <span>Home</span>
+            </BubblyLink>
+            <BubblyLink
+              colorStart="#162030"
+              colorEnd="#0d1117"
+              onClick={toggleSidebar}
+              to="/about"
+            >
+              <img src={require("../assets/image-about.png")} alt="home" />
+              <span>About</span>
+            </BubblyLink>
+            <BubblyLink
+              colorStart="#162030"
+              colorEnd="#0d1117"
+              onClick={toggleSidebar}
+              to="/stack"
+            >
+              <img src={require("../assets/image-stack.png")} alt="home" />
+              <span>Toolbox</span>
+            </BubblyLink>
+            <BubblyLink
+              colorStart="#162030"
+              colorEnd="#0d1117"
+              onClick={toggleSidebar}
+              to="/projects"
+            >
+              <img src={require("../assets/image-projects.png")} alt="home" />
+              <span>Projects</span>
+            </BubblyLink>
+            <></>
+          </nav>
+        </div>
       </div>
     </>
   );
 };
 
-  // const [navbar, setNavbar] = React.useState("invisible");
+// const [navbar, setNavbar] = React.useState("invisible");
 
-  // const handleHamburgerClick = () => {
-  //   setNavbar(navbar === "invisible" ? "" : "invisible");
-  // };
+// const handleHamburgerClick = () => {
+//   setNavbar(navbar === "invisible" ? "" : "invisible");
+// };
 
-  // return (
-  //   <header>
-      // <div
-      //   className="hamburger"
-      //   id="hamburger"
-      //   role="presentation"
-      //   onClick={handleHamburgerClick}
-      // >
-      //   <div className="slice" />
-      // </div>
-  //     <nav className={navbar}>
-  //       <ul className="navigator">
-  //         <li>
-  //           <NavLink onClick={handleHamburgerClick} to="/">
-  //             Home
-  //           </NavLink>
-  //         </li>
-  //         <li>
-  //           <NavLink onClick={handleHamburgerClick} to="/about">
-  //             About me
-  //           </NavLink>
-  //         </li>
-  //       </ul>
-  //     </nav>
-  //   </header>
-  // );
-  //};
+// return (
+//   <header>
+// <div
+//   className="hamburger"
+//   id="hamburger"
+//   role="presentation"
+//   onClick={handleHamburgerClick}
+// >
+//   <div className="slice" />
+// </div>
+//     <nav className={navbar}>
+//       <ul className="navigator">
+//         <li>
+//           <NavLink onClick={handleHamburgerClick} to="/">
+//             Home
+//           </NavLink>
+//         </li>
+//         <li>
+//           <NavLink onClick={handleHamburgerClick} to="/about">
+//             About me
+//           </NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   </header>
+// );
+//};
 
 export default Menu;
