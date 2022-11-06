542e78bb1cfe5bfe1eb91536335e5bceb72ae88d
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-11-05 20:04:21 -0600

    add accent image with logo

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,12 @@
 const Home = () => {
   return (
     <div className="pagecontainer">
-      <h1>Home</h1>
       <p>
         Welocme to My portfolio! I'm Alex, a full stack web developer from
         Monterrey, Mexico. Here you can find some info about me and my projects.
       </p>
+
+      <img className="home-image" src={require("../assets/home-image.jpg")} alt="computer" />
 
       <p>
         I'm always interested in hearing about new projects, so if you'd like to
