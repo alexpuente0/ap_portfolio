88f4f65abca00acb7d64854dcc7913714e0f915a
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-12-14 13:27:01 -0600

    inverted data order

--- src/components/data.js
+++ src/components/data.js
@@ -1,110 +1,22 @@
 const projectDetails = [
-  {
-    id: 1,
-    title: "Bombay Bicicle Club - Challenge Page",
-    description:
-      "The very first website i made, its a fanpage for the band Bombay Bicycle Club. It is made only using HTML and CSS, and its quite simple. It was created as a challenge, and the time to complete the project was limited to 2 hours or less",
-    image: "http://live.staticflickr.com/65535/52442831290_73fd0f471f_c.jpg",
-    link: "http://github.com/alexpuente0/challenges",
-  },
+  // {
+  //   id: 1,
+  //   title: "Rent-A-Green",
+  //   description:
+  //     "This application allow users to book an appointment to test an electric car. The homepage is a showcase of the available vehicles. You can add new cars, view the details of each one, and book an appointment for renting a car, among other features. This App is made with React with a propietary RoR API.",
+  //   image: "https://i.imgur.com/0Z0Z7Zu.png",
+  //   link: "https://github.com/alexpuente0/final-capstone-frontend",
+  // },
   {
     id: 2,
-    title: "My First Portfolio",
+    title: "Budget Wizard",
     description:
-      "This is a generic portfolio website. It was built as an assignment using basic HTML, CSS and Javascript. Was based off a generic Figma template. It was a great learning experience, as it helped me translate from UI/UX design to a functional website.",
-    image: "https://live.staticflickr.com/65535/52484612786_3b1f7a0815_c.jpg",
-    link: "https://github.com/alexpuente0/My-Portfolio",
+      "An object-based personal finance Mobile App where each user can log in and register their expenses in different categories or groups.",
+    image: "https://live.staticflickr.com/65535/52489033286_7babcd321a_o.png",
+    link: "https://github.com/alexpuente0/budget-wizard",
   },
   {
     id: 3,
-    title: "Unlimited Love Launch Party",
-    description:
-      'This is a fan-made website celebrating the mock-launch party for the 2022 album "Unlimited Love" by the Red Hot Chili Peppers. Most of the assets were made by me using photoshop.',
-    image: "https://live.staticflickr.com/65535/52487976497_eb08e7906f_c.jpg",
-    link: "https://github.com/alexpuente0/Capstone_festival",
-  },
-  {
-    id: 4,
-    title: "Awesome Books",
-    description:
-      "This Single Page Application was built using Javascript, HTML and CSS. It allows the user to add and remove books from a list. Book list is saved in local storage.",
-    image: "https://live.staticflickr.com/65535/52488025802_b2420753ac_c.jpg",
-    link: "https://github.com/alexpuente0/awesome_books_es6",
-  },
-  {
-    id: 5,
-    title: "To-do List",
-    description:
-      "An interactive list of tasks where you can add tasks, mark them as complete and edit them. Made with Javascript.",
-    image: "https://live.staticflickr.com/65535/52488540476_afbb3a7227_c.jpg",
-    link: "https://github.com/alexpuente0/todo_list",
-  },
-  {
-    id: 6,
-    title: "Leaderboard",
-    description:
-      'A simple little fun "game" where you submit your name and get assigned a random Score. The scores are listed in descending order on a leaderboard, and you can see the top 10 scores or scroll down for the rest. Data is stored in an API.',
-    image: "https://live.staticflickr.com/65535/52489024100_32f04571c8_c.jpg",
-    link: "https://github.com/alexpuente0/Leaderboard",
-  },
-  {
-    id: 7,
-    title: "MathMagicians",
-    description:
-      "This project its a Single Page Application which includes a homepage, an interactive calculator and a math-related quote. Made with React",
-    image: "https://live.staticflickr.com/65535/52488663521_525d5425a6_o.png",
-    link: "https://github.com/alexpuente0/mathmagicians",
-  },
-  {
-    id: 8,
-    title: "BookStore CMS",
-    description:
-      "This project its a Single Page Application to keep track of which books you've started or finished reading. Information is stored on an external API.",
-    image: "https://live.staticflickr.com/65535/52489233735_306f90f4d4_o.png",
-    link: "https://github.com/alexpuente0/bookstore-react",
-  },
-  {
-    id: 9,
-    title: "Space Traveler's Hub",
-    description:
-      "This React WebApp allows the user to book rockets and sign up for missions from the SpaceX API.",
-    image: "https://live.staticflickr.com/65535/52488780106_c631da56d8_o.png",
-    link: "https://github.com/alexpuente0/react-spacex",
-  },
-  {
-    id: 10,
-    title: "Air Quailty Monitor",
-    description:
-      "Single Page app made with React using a Global Air Quality API. It allows the user to search for a city and get the current air quality index.",
-    image: "https://live.staticflickr.com/65535/52488304377_bb454a8409_o.png",
-    link: "https://github.com/alexpuente0/air-quality-monitor",
-  },
-  {
-    id: 11,
-    title: "School Library",
-    description:
-      "This is a school library app, created using OOP on Ruby, a tool to record what books are in the library and who borrows them. This app allows you to add books, students, teachers, save a record of who borrowed each book and when.",
-    image: "https://live.staticflickr.com/65535/52488343537_94a20e2a74_o.png",
-    link: "https://github.com/alexpuente0/school_library_ruby",
-  },
-  {
-    id: 12,
-    title: "Item List",
-    description:
-      "An object-based personal list where you can save your favorite thing information ordered by author, lables and genre.",
-    image: "https://live.staticflickr.com/65535/52488858076_ea98b7035f_o.png",
-    link: "https://github.com/alexpuente0/list_of_things",
-  },
-  {
-    id: 13,
-    title: "Blog Page",
-    description:
-      "Generic Blog website created using Ruby on Rails. After LogIn, user can create posts, as well as comment and like posts by other users. This App can also be used as an API to post a comment or get a post's info.",
-    image: "https://live.staticflickr.com/65535/52489441983_2877a946d6_o.png",
-    link: "https://github.com/alexpuente0/rails_blog",
-  },
-  {
-    id: 14,
     title: "Recipes App",
     description:
       "An object-based personal Recipe app where each user can log in and enter their recipes while using an ingredients inventory and a shopping list for the missing ones. Made with Ruby on Rails",
@@ -112,21 +24,109 @@ const projectDetails = [
     link: "https://github.com/alexpuente0/recipes_rails",
   },
   {
-    id: 15,
-    title: "Budget Wizard",
+    id: 4,
+    title: "Blog Page",
     description:
-      "An object-based personal finance Mobile App where each user can log in and register their expenses in different categories or groups.",
-    image: "https://live.staticflickr.com/65535/52489033286_7babcd321a_o.png",
-    link: "https://github.com/alexpuente0/budget-wizard",
+      "Generic Blog website created using Ruby on Rails. After LogIn, user can create posts, as well as comment and like posts by other users. This App can also be used as an API to post a comment or get a post's info.",
+    image: "https://live.staticflickr.com/65535/52489441983_2877a946d6_o.png",
+    link: "https://github.com/alexpuente0/rails_blog",
   },
-  // {
-  //   id: 16,
-  //   title: "Rent-A-Green",
-  //   description:
-  //     "This application allow users to book an appointment to test an electric car. The homepage is a showcase of the available vehicles. You can add new cars, view the details of each one, and book an appointment for renting a car, among other features. This App is made with React with a propietary RoR API.",
-  //   image: "https://i.imgur.com/0Z0Z7Zu.png",
-  //   link: "https://github.com/alexpuente0/final-capstone-frontend",
-  // },
+  {
+    id: 5,
+    title: "Item List",
+    description:
+      "An object-based personal list where you can save your favorite thing information ordered by author, lables and genre.",
+    image: "https://live.staticflickr.com/65535/52488858076_ea98b7035f_o.png",
+    link: "https://github.com/alexpuente0/list_of_things",
+  },
+  {
+    id: 6,
+    title: "School Library",
+    description:
+      "This is a school library app, created using OOP on Ruby, a tool to record what books are in the library and who borrows them. This app allows you to add books, students, teachers, save a record of who borrowed each book and when.",
+    image: "https://live.staticflickr.com/65535/52488343537_94a20e2a74_o.png",
+    link: "https://github.com/alexpuente0/school_library_ruby",
+  },
+  {
+    id: 7,
+    title: "Air Quailty Monitor",
+    description:
+      "Single Page app made with React using a Global Air Quality API. It allows the user to search for a city and get the current air quality index.",
+    image: "https://live.staticflickr.com/65535/52488304377_bb454a8409_o.png",
+    link: "https://github.com/alexpuente0/air-quality-monitor",
+  },
+  {
+    id: 8,
+    title: "Space Traveler's Hub",
+    description:
+      "This React WebApp allows the user to book rockets and sign up for missions from the SpaceX API.",
+    image: "https://live.staticflickr.com/65535/52488780106_c631da56d8_o.png",
+    link: "https://github.com/alexpuente0/react-spacex",
+  },
+  {
+    id: 9,
+    title: "BookStore CMS",
+    description:
+      "This project its a Single Page Application to keep track of which books you've started or finished reading. Information is stored on an external API.",
+    image: "https://live.staticflickr.com/65535/52489233735_306f90f4d4_o.png",
+    link: "https://github.com/alexpuente0/bookstore-react",
+  },
+  {
+    id: 10,
+    title: "MathMagicians",
+    description:
+      "This project its a Single Page Application which includes a homepage, an interactive calculator and a math-related quote. Made with React",
+    image: "https://live.staticflickr.com/65535/52488663521_525d5425a6_o.png",
+    link: "https://github.com/alexpuente0/mathmagicians",
+  },
+  {
+    id: 11,
+    title: "Leaderboard",
+    description:
+      'A simple little fun "game" where you submit your name and get assigned a random Score. The scores are listed in descending order on a leaderboard, and you can see the top 10 scores or scroll down for the rest. Data is stored in an API.',
+    image: "https://live.staticflickr.com/65535/52489024100_32f04571c8_c.jpg",
+    link: "https://github.com/alexpuente0/Leaderboard",
+  },
+  {
+    id: 12,
+    title: "To-do List",
+    description:
+      "An interactive list of tasks where you can add tasks, mark them as complete and edit them. Made with Javascript.",
+    image: "https://live.staticflickr.com/65535/52488540476_afbb3a7227_c.jpg",
+    link: "https://github.com/alexpuente0/todo_list",
+  },
+  {
+    id: 13,
+    title: "Awesome Books",
+    description:
+      "This Single Page Application was built using Javascript, HTML and CSS. It allows the user to add and remove books from a list. Book list is saved in local storage.",
+    image: "https://live.staticflickr.com/65535/52488025802_b2420753ac_c.jpg",
+    link: "https://github.com/alexpuente0/awesome_books_es6",
+  },
+  {
+    id: 14,
+    title: "Unlimited Love Launch Party",
+    description:
+      'This is a fan-made website celebrating the mock-launch party for the 2022 album "Unlimited Love" by the Red Hot Chili Peppers. Most of the assets were made by me using photoshop.',
+    image: "https://live.staticflickr.com/65535/52487976497_eb08e7906f_c.jpg",
+    link: "https://github.com/alexpuente0/Capstone_festival",
+  },
+  {
+    id: 15,
+    title: "My First Portfolio",
+    description:
+      "This is a generic portfolio website. It was built as an assignment using basic HTML, CSS and Javascript. Was based off a generic Figma template. It was a great learning experience, as it helped me translate from UI/UX design to a functional website.",
+    image: "https://live.staticflickr.com/65535/52484612786_3b1f7a0815_c.jpg",
+    link: "https://github.com/alexpuente0/My-Portfolio",
+  },
+  {
+    id: 16,
+    title: "Bombay Bicicle Club - Challenge Page",
+    description:
+      "The very first website i made, its a fanpage for the band Bombay Bicycle Club. It is made only using HTML and CSS, and its quite simple. It was created as a challenge, and the time to complete the project was limited to 2 hours or less",
+    image: "http://live.staticflickr.com/65535/52442831290_73fd0f471f_c.jpg",
+    link: "http://github.com/alexpuente0/challenges",
+  },
 ];
 
 export default projectDetails;
