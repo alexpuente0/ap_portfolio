e1ba3d4600ac6aa6338f6ea08afc3c550b653e8e
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2023-02-02 12:00:41 -0600

    remove unused code

--- src/components/menu.js
+++ src/components/menu.js
@@ -62,38 +62,4 @@ const Menu = () => {
   );
 };
 
-// const [navbar, setNavbar] = React.useState("invisible");
-
-// const handleHamburgerClick = () => {
-//   setNavbar(navbar === "invisible" ? "" : "invisible");
-// };
-
-// return (
-//   <header>
-// <div
-//   className="hamburger"
-//   id="hamburger"
-//   role="presentation"
-//   onClick={handleHamburgerClick}
-// >
-//   <div className="slice" />
-// </div>
-//     <nav className={navbar}>
-//       <ul className="navigator">
-//         <li>
-//           <NavLink onClick={handleHamburgerClick} to="/">
-//             Home
-//           </NavLink>
-//         </li>
-//         <li>
-//           <NavLink onClick={handleHamburgerClick} to="/about">
-//             About me
-//           </NavLink>
-//         </li>
-//       </ul>
-//     </nav>
-//   </header>
-// );
-//};
-
 export default Menu;
