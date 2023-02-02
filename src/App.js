3f7b5b0e4713eef9f7f54bd4f2f8754111d56df4
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2023-02-02 12:00:51 -0600

    Remap router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/home';
-import About from './components/about';
-import Menu from './components/menu';
-import Projects from './components/projects';
-import Toolbox from './components/stack';
-import { BubblyContainer } from 'react-bubbly-transitions';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Menu from "./components/menu";
+import Projects from "./components/projects";
+import Toolbox from "./components/stack";
+import { BubblyContainer } from "react-bubbly-transitions";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <BubblyContainer />
         <Menu />
+
+        <BubblyContainer />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path='/stack' element={<Toolbox />} />
+          <Route path="/stack" element={<Toolbox />} />
         </Routes>
       </Router>
     </div>
